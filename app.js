4fbb36e2b237d67280ea815450b1fb6c956334b8
nulltask <nulltask@gmail.com>
2013-04-16 21:37:52 +0900

    update osc message address

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.post('/plays/:timestamp/pictures', function(req, res){
   //TODO gifの保存
   //mimetypeで jpg gifで振り分け
   var file = req.files.upfile;
-  console.log('file.path:',file.path);
+  // console.log('file.path:',file.path);
   console.log('req.files', req.files);
   res.send(200);
   mediator.uploaded();
