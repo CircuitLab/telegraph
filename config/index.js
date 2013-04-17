c489d13b08a3292e43e0bc4991e18c82fc1ee57a
nulltask <nulltask@gmail.com>
2013-04-17 18:25:17 +0900

    tweak config

--- config/index.js
+++ config/index.js
@@ -8,12 +8,12 @@ var fs = require('fs')
   , env = require('../lib/env')
   , config = {};
 
-console.log(__dirname);
 fs.readdirSync(__dirname).forEach(function(filename) {
   if (!/\.json/.test(filename)) return;
   var name = path.basename(filename, '.json');
   config[name] = require( path.join(__dirname, filename) );
 });
 
-config.tumblr = require( path.join(__dirname, 'secrets','tumblr.json') );
+config.tumblr = require('./secrets/tumblr.json');
+
 module.exports = config;
