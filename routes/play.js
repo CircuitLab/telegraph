456efb9a0b86e738aaaad7ac1edf91b620e2e352
daichi <prinum101@gmail.com>
2013-04-18 09:37:32 +0900

    show

--- routes/play.js
+++ routes/play.js
@@ -1,11 +1,22 @@
 var config = require('../config');
+
 var models = require('../models');
 var Play = models.Play;
+var db = models.db;
 
 //TODO show play gif
 exports.show = function(req, res) {
-  var timestamp = req.query['timestamp'];
-  res.render('play/index', { title: 'デバタイプ', letters: letters });
+  var timestamp = req.params.timestamp;
+  Play.findOne({timestamp: timestamp}, function(err, play){
+    if(err) return res.send(404);
+    res.render('play/show', 
+      { 
+        title: 'デバタイプ', 
+        message: play.message,
+        imgUrl: play.gifUrl(),
+    });
+  });
+  
 };
 
 exports.animation = function(req, res){
